d6155a16e0a8d4384bc36347687abd62af086a8a
windson3 <windson3@gmail.com>
2026-06-01 11:04:29 -0300

    release: atualiza pacote InstTotem 2026-06-01 11:04

--- i.ps1
+++ i.ps1
@@ -3,6 +3,20 @@ param()
 
 $ErrorActionPreference = "Stop"
 
+function Test-IsAdministrator {
+    $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $principal = [Security.Principal.WindowsPrincipal]::new($identity)
+    return $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+$selfUri = "https://raw.githubusercontent.com/windson3/InstTotem/main/i.ps1"
+if (-not (Test-IsAdministrator)) {
+    $cacheBustSelf = [DateTimeOffset]::UtcNow.ToUnixTimeSeconds()
+    $selfCommand = "iwr -useb `"$selfUri?cb=$cacheBustSelf`" | iex"
+    Start-Process powershell.exe -Verb RunAs -ArgumentList @("-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", $selfCommand)
+    return
+}
+
 if ($PSVersionTable.PSEdition -ne "Core") {
     [Net.ServicePointManager]::SecurityProtocol = `
         [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
